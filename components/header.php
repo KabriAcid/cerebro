b4d9da29462ab7d4ce0df6bdbbf8ccf5a377d593
jugulde03 <kabriacid01@gmail.com>
2025-06-25 11:52:06 +0100

    commit

--- components/header.php
+++ components/header.php
@@ -10,14 +10,14 @@
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" href="/cerebro/assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="/cerebro/assets/css/style.css">
 
     <style>
         body {
-            font-family: 'Ubuntu', sans-serif;
+            font-family: 'Raleway', sans-serif;
         }
     </style>
 
