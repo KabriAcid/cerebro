a28b85a3291ba01c3015daee096104e8b24b1357
KabriAcid <kabriacid01@gmail.com>
2025-06-28 00:53:18 +0100

    Add avatar dropdown menu with enhanced styling and user options

--- components/dashboard-navbar.php
+++ components/dashboard-navbar.php
@@ -1,9 +1,18 @@
- <!-- Avatar with Dropdown -->
- <div class="avatar-container position-relative">
-     <img src="../assets/img/avatar.jpg" alt="Avatar" class="avatar" onclick="toggleDropdown()" />
-     <div class="dropdown-menu" id="avatarDropdown">
-         <a href="../profile/edit-profile.php" class="dropdown-item">Edit Profile</a>
-         <a href="../settings.php" class="dropdown-item">Settings</a>
-         <a href="../logout.php" class="dropdown-item">Logout</a>
-     </div>
- </div>
+<!-- Avatar with Dropdown -->
+<header class="dashboard-navbar" style="position: relative;">
+    <div class="dropdown-container">
+        <div class="avatar-container">
+            <img src="../assets/img/avatar.png" alt="Avatar" class="avatar" onclick="toggleDropdown()" />
+        </div>
+        <div class="dropdown-menu" id="avatarDropdown">
+            <div class="dropdown-header">
+                <span class="dropdown-email">kabriacid01@gmail.com</span>
+            </div>
+            <a href="../profile/edit-profile.php" class="dropdown-item">Upgrade Plan</a>
+            <a href="../settings.php" class="dropdown-item">Customize ChatGPT</a>
+            <a href="../settings.php" class="dropdown-item">Settings</a>
+            <a href="../help.php" class="dropdown-item">Help</a>
+            <a href="../logout.php" class="dropdown-item">Log Out</a>
+        </div>
+    </div>
+</header>
